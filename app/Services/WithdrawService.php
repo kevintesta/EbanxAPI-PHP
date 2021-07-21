e9f4bfdbcb4fdb074ea1408acecba0c82e81e700
kevintesta <66317451+kevintesta@users.noreply.github.com>
2021-07-21 10:38:59 -0300

    Adding feature 1

--- app/Services/WithdrawService.php
+++ app/Services/WithdrawService.php
@@ -5,7 +5,8 @@ namespace App\Services;
 use Illuminate\Http\Request;
 use App\Models\Account;
 
-class WithdrawService {
+class WithdrawService 
+{
     public function execute(Request $request) {
 
         $account = Account::find($request->origin);
