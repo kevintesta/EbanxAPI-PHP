ae3331f6c4f21116740aefac6ff31a9351c6e001
kevintesta <66317451+kevintesta@users.noreply.github.com>
2021-07-21 11:49:25 -0300

    Commit 1 - feature 1

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Account extends Model {
+class Account extends Model 
+{
 
     /**
      * The attributes that are mass assignable.
